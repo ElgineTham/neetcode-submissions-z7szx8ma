a8067d267e6d6fe948fd997ae8ad360c3420e1cb
Elgine Theo Thamtoro <101312263+ElgineTham@users.noreply.github.com>
2026-04-17 02:57:43 -0400

    Add: climbing-stairs - submission-36

---
+++ Data Structures & Algorithms/climbing-stairs/submission-36.py
@@ -0,0 +1,18 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        step_1 = 2
+        step_2 = 1
+
+        for i in range(n-1, -1, -1):
+            if i+1 >= n or i+2 >= n:
+                continue
+            
+            temp = step_1
+            step_1 = step_1 + step_2
+            step_2 = temp
+        
+        return step_1
