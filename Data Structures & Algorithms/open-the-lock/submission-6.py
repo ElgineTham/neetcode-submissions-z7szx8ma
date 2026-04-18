3f1be8256410915c7b8faae1c5fc4d5686fa695c
Elgine Theo Thamtoro <101312263+ElgineTham@users.noreply.github.com>
2026-04-18 01:59:35 -0400

    Add: open-the-lock - submission-6

---
+++ Data Structures & Algorithms/open-the-lock/submission-6.py
@@ -0,0 +1,31 @@
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        if "0000" in deadends:
+            return -1
+            
+        seen = set()
+        q = deque()
+        q.append("0000")
+        seen.add("0000")
+        turns = 0
+
+        while q:
+            for i in range(len(q)):
+                comb = q.popleft()
+                if comb == target:
+                    return turns
+                for j in range(4):
+                    dig_1 = str((int(comb[j]) + 1) % 10)
+                    dig_2 = str((int(comb[j]) - 1 + 10) % 10)
+                    child_1 = comb[:j] + dig_1 + comb[j+1:]
+                    child_2 = comb[:j] + dig_2 + comb[j+1:]
+                    if child_1 not in seen and child_1 not in deadends:
+                        q.append(child_1)
+                        seen.add(child_1)
+                    if child_2 not in seen and child_2 not in deadends:
+                        q.append(child_2)
+                        seen.add(child_2)
+            
+            turns += 1
+        
+        return -1
