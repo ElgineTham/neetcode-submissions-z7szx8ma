eea47645e8dbf3595ec3336ff9dd1cdee8e096aa
Elgine Theo Thamtoro <101312263+ElgineTham@users.noreply.github.com>
2026-04-21 17:46:58 -0400

    Add: jump-game-ii - submission-7

---
+++ Data Structures & Algorithms/jump-game-ii/submission-7.py
@@ -0,0 +1,21 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        if len(nums) < 2:
+            return 0
+        l, r = 0, 0
+
+        jumps = 0
+        while r < len(nums):
+            jumps += 1
+            jump = 0
+
+            for i in range(l, r+1):
+                jump = max(jump, nums[i])
+            
+            l = r + 1
+            r += jump
+
+            if r >= len(nums)-1:
+                break
+            
+        return jumps
