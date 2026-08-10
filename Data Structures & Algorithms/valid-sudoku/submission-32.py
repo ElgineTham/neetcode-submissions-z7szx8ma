e2616f6857526bb5a3138b543259bc1c9bc5c6f5
Elgine T <101312263+ElgineTham@users.noreply.github.com>
2026-08-10 14:54:39 -0700

    Add: valid-sudoku - submission-32

---
+++ Data Structures & Algorithms/valid-sudoku/submission-32.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows, cols, reg = defaultdict(set), defaultdict(set), defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                element = board[r][c]
+                
+                if element == ".":
+                    continue
+
+                if (element in rows[r] 
+                    or element in cols[c] 
+                    or element in reg[(r//3, c//3)]):
+                    return False
+                
+                rows[r].add(element)
+                cols[c].add(element)
+                reg[(r//3, c//3)].add(element)
+        
+        return True
