afaf2832ea73d00a979945d4b11f857a1b587a22
Elgine T <101312263+ElgineTham@users.noreply.github.com>
2026-08-10 15:11:56 -0700

    Add: anagram-groups - submission-29

---
+++ Data Structures & Algorithms/anagram-groups/submission-29.py
@@ -0,0 +1,17 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashMap = {}  # key is num of char in word
+                      # value is list of anagrams
+        for word in strs:
+            count = [0] * 26
+            for char in word:
+                count[ord(char) - ord('a')] += 1
+                
+            if tuple(count) not in hashMap:
+                tempList = []
+                tempList.append(word)
+                hashMap[tuple(count)] = tempList
+            else:
+                hashMap[tuple(count)].append(word)
+        
+        return list(hashMap.values())
